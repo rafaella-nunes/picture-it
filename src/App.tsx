0518b2ad3c7dc6aeedeff8b034369a1430f02748
rafaella-nunes <faellanunes01@gmail.com>
2023-12-08 16:54:07 -0300

    chore: set file structure up

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
+import { Routes, Route } from 'react-router-dom';
 import './globals.css';
+import SignInForm from './_auth/forms/SignInForm';
+import SignUpForm from './_auth/forms/SignUpForm';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
+import { Home } from './_root/pages';
 
 const App = () => {
     return (
-        <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <main className='flex h-screen'>
+        <Routes>
+            {/* public routes */}
+            <Route element={<AuthLayout/>}>
+                <Route path='/sign-in' element={<SignInForm />}/>
+                <Route path='/sign-up' element={<SignUpForm />}/>
+            </Route>
+
+            {/* private routes */}
+            <Route element={<RootLayout/>}>
+                <Route element={<Home />} />
+            </Route>
+        </Routes>
+      </main>
     )
 }
 
