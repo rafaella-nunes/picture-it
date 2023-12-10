94c3258cfc48e973f0eb4e6152c000d10706aa04
rafaella-nunes <faellanunes01@gmail.com>
2023-12-10 14:57:19 -0300

    chore: implements is auth

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Outlet, Navigate } from 'react-router-dom';
 
 const AuthLayout = () => {
+  const isAunthenticated = false;
   return (
-    <div>AuthLayout</div>
+    <>
+    {isAunthenticated ? (
+      <Navigate to="/" />
+      ) : (
+        <> 
+          <section>
+            <Outlet />
+          </section>
+        </>
+      )}
+    </>
   )
 }
 
