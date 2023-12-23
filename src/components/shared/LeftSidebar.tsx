0392fb8a4bc048cc5080d842581625eb3d760f51
rafaella-nunes <faellanunes01@gmail.com>
2023-12-23 12:32:51 -0300

    feat: add sidebar

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -1,9 +1,13 @@
 import React, { useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, NavLink, useLocation, useNavigate } from 'react-router-dom';
 import { useSignOutAccount } from '@/lib/react-query/queriesAndMutations';
 import { useUserContext } from '@/context/authContext';
+import { sidebarLinks } from '@/constants';
+import { INavLink } from '@/types';
+import { Button } from '../ui/button';
 
 const LeftSidebar = () => {
+  const { pathname } = useLocation();
   const { mutate: signOut, isSuccess } = useSignOutAccount();
     const navigate = useNavigate();
     const { user } = useUserContext();
@@ -35,7 +39,35 @@ const LeftSidebar = () => {
             <p className='small-regular text-light-3'>@{user.username}</p>
           </div>
         </Link>
+
+        <ul className='flex flex-col gap-6'>
+          {sidebarLinks.map((link: INavLink) => {
+            const isActive = pathname === link.route;
+
+            return (
+              <li key={link.label} className={`leftsidebar-link group ${isActive && 'bg-primary-500'}`}>
+                <NavLink to={link.route}
+                className='flex gap-4 items-center p-4'>
+                  <img 
+                  src={link.imgURL}
+                  alt={link.label}
+                  className={`group-hover:invert-white ${isActive && 'invert-white'}`}
+                  />
+                  {link.label}
+                </NavLink>
+              </li>
+            )
+          })}
+        </ul>
       </div>
+
+      <Button variant="ghost" className='shad-button_ghost' onClick = { () => signOut()}>
+        <img 
+          src='/public/assets/icons/logout.svg'
+          alt='logout'
+        />
+        <p className='small-medium lg:base-medium'>Logout</p>
+      </Button>
     </nav>
   )
 }
