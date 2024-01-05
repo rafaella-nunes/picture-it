21a1712c52dc59fa1bf30fa5e8946b3aa7cbdc1f
rafaella-nunes <faellanunes01@gmail.com>
2024-01-04 21:11:51 -0300

    feat: add like and save on the ui

--- src/components/shared/PostCard.tsx
+++ src/components/shared/PostCard.tsx
@@ -2,6 +2,7 @@ import { useUserContext } from "@/context/authContext";
 import { formatDateString, multiFormatDateString } from "@/lib/utils";
 import { Models } from "appwrite"
 import { Link } from "react-router-dom";
+import PostStats from "./PostStats";
 
 type PostCardProps = {
     post: Models.Document;
@@ -60,6 +61,8 @@ const PostCard = ({ post }: PostCardProps) => {
             alt="post image"
             />
         </Link>
+
+        <PostStats post={post} userId = {user.id} />
     </div>
   )
 }
