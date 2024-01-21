54979c2636d1b6df5dc4ea0a9d8bb57c917bf425
rafaella-nunes <faellanunes01@gmail.com>
2024-01-20 22:14:05 -0300

    feat: implements search bar

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { Input } from '@/components/ui/input'
+import { useState } from 'react'
 
 const Explore = () => {
+  const [searchValue, setSearchValue] = useState('')
+
   return (
-    <div>Explore</div>
+    <div className='explore-container'>
+      <div className='explore-inner_container'>
+        <h2 className='h3-bold md:h2-bold w-full'>Search Posts</h2>
+        <div className='flex gap-1 px-4 w-full rounded-lg bg-dark-4'>
+          <img 
+          src='/public/assets/icons/search.svg'
+          width={24}
+          height={24}
+          alt='search'/>
+          <Input 
+          type='text'
+          placeholder='Search'
+          className='explore-search'
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}/>
+        </div>
+      </div>
+    </div>
   )
 }
 
