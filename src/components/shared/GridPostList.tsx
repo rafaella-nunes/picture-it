d3daabb6417d0468681523fe3839d98eb87ffa44
rafaella-nunes <faellanunes01@gmail.com>
2024-01-24 16:00:16 -0300

    feat: explore page shows post images

--- src/components/shared/GridPostList.tsx
+++ src/components/shared/GridPostList.tsx
@@ -1,6 +1,24 @@
-const GridPostList = () => {
+import { useUserContext } from "@/context/authContext"
+import { Models } from "appwrite"
+import { Link } from "react-router-dom"
+
+type GridPostListProps = {
+  posts: Models.Document[]
+}
+
+const GridPostList = ({ posts }: GridPostListProps) => {
+  const { user } = useUserContext();
   return (
-    <div>GridPostList</div>
+    <ul className="grid-container">
+      {posts.map((post) => (
+        <li key={post.$id} className="relative min-w-80 h-80">
+          <Link>
+            <img 
+            src={post.imageUrl}/>
+          </Link>
+        </li>
+      ))}
+    </ul>
   )
 }
 
