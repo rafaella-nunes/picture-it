99bf093dbd53501a22ee031a5edff3a37bf6c9ad
rafaella-nunes <faellanunes01@gmail.com>
2024-03-04 14:16:23 -0300

    fix: fixes sing up

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -3,54 +3,53 @@ import { ID, Query } from 'appwrite';
 import { account, appwriteConfig, avatars, databases, storage } from "./config";
 
 export async function createUserAccount(user: INewUser) {
-    try {
-        const newAccount = await account.create(
-            ID.unique(),
-            user.email,
-            user.password,
-            user.name,
-        );
+  try {
+    const newAccount = await account.create(
+      ID.unique(),
+      user.email,
+      user.password,
+      user.name
+    );
 
-        if(!newAccount) throw Error;
+    if (!newAccount) throw Error;
 
-        const avatarUrl = avatars.getInitials(user.name);
+    const avatarUrl = avatars.getInitials(user.name);
 
-        const newUser = await saveUsertoDB({
-            accountId: newAccount.$id,
-            email: newAccount.name,
-            name: newAccount.email,
-            username: user.username,
-            imageUrl: avatarUrl,
-        })
-        return newUser;
+    const newUser = await saveUserToDB({
+      accountId: newAccount.$id,
+      name: newAccount.name,
+      email: newAccount.email,
+      username: user.username,
+      imageUrl: avatarUrl,
+    });
 
-    } catch (error){
-        console.log(error);
-        return error;
-    }
+    return newUser;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
 }
 
-export async function saveUsertoDB(user: {
-    accountId: string;
-    email: string;
-    name: string;
-    imageUrl: URL;
-    username?: string;
+export async function saveUserToDB(user: {
+  accountId: string;
+  email: string;
+  name: string;
+  imageUrl: URL;
+  username?: string;
 }) {
-    try{
-        const newUser = await databases.createDocument(
-            appwriteConfig.databaseId,
-            appwriteConfig.userCollectionId,
-            ID.unique(),
-            user,
-        )
+  try {
+    const newUser = await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      ID.unique(),
+      user
+    );
 
-        return newUser;
-    } catch(error){
-        console.log(error);
-    }
+    return newUser;
+  } catch (error) {
+    console.log(error);
+  }
 }
-
 export async function signInAccount(user: {
     email: string; 
     password: string;
